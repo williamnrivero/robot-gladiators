47d454c5ca0c5706a959c4bb4972edc2afef28e0
williamnrivero <williamnrivero@gmail.com>
2021-03-23 18:03:36 -0600

    final changes to start and end game features

--- assets/js/game.js
+++ assets/js/game.js
@@ -87,9 +87,29 @@ var startGame = function() {
       break;
     }
   }
-  //play again
-  startGame();
+  // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+  endGame();
 };
 
 // start the game when the page loads
 startGame();
+
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+}
+// ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
